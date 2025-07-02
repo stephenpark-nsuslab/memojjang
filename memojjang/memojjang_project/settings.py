f764f4d83d7a0426c6bd780fbddcd214c7efc4e9
stephen <stephen.park@nsuslab.com>
2025-07-02 15:05:15 +0900

    11단계: 프로덕션 준비 완료

--- memojjang/memojjang_project/settings.py
+++ memojjang/memojjang_project/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# 정적 파일이 수집될 디렉토리 경로
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
