ce983ef92e717eb9cfc8756c20aa8f98d82e1c8a
stephen <stephen.park@nsuslab.com>
2025-07-02 14:56:43 +0900

    6단계: 템플릿 및 UI 구현 완료

--- memojjang/memojjang_project/urls.py
+++ memojjang/memojjang_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
+    path('memos/', include('apps.memos.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
